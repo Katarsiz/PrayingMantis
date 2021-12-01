2ee564ae0b39eca711f26b72cc679261322ef490
JulioRojas <katarsis@ciencias.unam.mx>
2021-12-01 13:49:04 -0600

    Adds audio effects.
    Audio effects are needed for most entities and objects.
    
    Adds audio effects for entities attack, damage and death.
    
    Also adds sfx of open doors.
    
    Adds some scripts for audio managing in general.

---
+++ Assets/src/audio/AudioOSPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOSPlayer : MonoBehaviour {
+
+    public AudioClip[] clips;
+
+    public void PlayOneShotAC(int index) {
+        AudioEventManager.PlayOneShotAudioClip(clips[index ]);
+    }
+}
